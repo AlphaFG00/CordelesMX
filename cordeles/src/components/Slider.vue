6532b502c8f762e9b8d4f0000a29042975906ab4
jehu nieves <jehunieves@gmail.com>
2020-07-24 20:57:27 -0500

    video en slider y formato responsive en ello

--- cordeles/src/components/Slider.vue
+++ cordeles/src/components/Slider.vue
@@ -1,57 +1,63 @@
 <template>
 <div>
-    
-      <b-carousel
-        id="carousel-1"
-        v-model="slide"
-        :interval="4000"
-        controls
-        indicators
-        background="#ababab"
-        img-width="1024"
-        img-height="480"
-        style="text-shadow: 1px 1px 2px #333;"
-        @sliding-start="onSlideStart"
-        @sliding-end="onSlideEnd"
-      >
-        <!-- Text slides with image -->
-        <b-carousel-slide
-          caption="First slide"
-          text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-          img-src="https://picsum.photos/1024/480/?image=52"
-        ></b-carousel-slide>
-
-        <!-- Slides with custom text -->
-        <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-          <h1>Hello world!</h1>
-        </b-carousel-slide>
-
-        <!-- Slides with image only -->
-        <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
-
-        <!-- Slides with img slot -->
-        <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-        <b-carousel-slide>
-          <template v-slot:img>
-            <img
-              class="d-block img-fluid w-100"
-              width="1024"
-              height="480"
-              src="https://picsum.photos/1024/480/?image=55"
-              alt="image slot"
+    <b-container>
+      <b-row class="justify-content-center">
+        <b-col md="8" fluid="sm" >
+          <b-carousel
+            id="carousel-1"
+            v-model="slide"
+            :interval="4000"
+            controls
+            indicators
+            background="#ababab"
+            img-width="1024"
+            img-height="420"
+            style="text-shadow: 3px 3px 3px #000;"
+            @sliding-start="onSlideStart"
+            @sliding-end="onSlideEnd"
+          >
+            <!-- Text slides with image -->
+            <b-carousel-slide
+              caption="Cordeles"
+              text="Amplia variedad de cordeles"
             >
-          </template>
-        </b-carousel-slide>
+            <template v-slot:img>
+                <img
+                  class="d-block  w-100"
+                  width="720"
+                  height="320"
+                  src="https://i.imgur.com/EdKo3hE.gif"
+                  alt="cordeles"
+                >
+              </template>
+            </b-carousel-slide>
 
-        <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-        <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-            a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-          </p>
-        </b-carousel-slide>
-      </b-carousel>
-    
+            <b-carousel-slide>
+              <h2>Cable</h2>
+              <template v-slot:img>
+                <img 
+                src="@/assets/productos/Cable.jpg"
+                width="720"
+                height="320"
+                alt="Imagen 1"/>
+              </template>
+            </b-carousel-slide>
+
+            <b-carousel-slide>
+              <h2>Polipropileno</h2>
+              <template v-slot:img>
+                <img 
+                src="@/assets/productos/Cable_polipropileno.jpg"
+                width="720"
+                height="320"
+                alt="Imagen 2"/>
+              </template>
+            </b-carousel-slide>
+
+          </b-carousel>
+        </b-col>
+      </b-row>
+    </b-container>
     <br>
 </div>
 </template>
@@ -61,7 +67,7 @@
     data() {
       return {
         slide: 0,
-        sliding: null
+        sliding: null,
       }
     },
     methods: {
@@ -69,7 +75,7 @@
         this.sliding = true
       },
       onSlideEnd() {
-        this.sliding = false
+        this.sliding = true
       }
     }
   }
