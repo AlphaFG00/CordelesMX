7a04584b5efd5da26a06ffc9d58bfec81e6e86a4
jehu nieves <jehunieves@gmail.com>
2020-08-20 20:16:02 -0500

    Fix: details

--- src/views/Contacto.vue
+++ src/views/Contacto.vue
@@ -1,19 +1,30 @@
 <template>
     <div class="contacto">
       <b-container class="focus-contacto mt-3 mb-3 p-5">
-        <h1 class="cssanimation titulo">Contacto</h1>  
+        <!-- <h1 class="cssanimation titulo">Contacto</h1> -->
+
+        <div id="container">
+        Amarrando Buenos
+        <div id="flip">
+          <div><div>Productos</div></div>
+          <div><div>Negocios</div></div>
+          <div><div>Tratos</div></div>
+        </div>
+        Con Nuestros Clientes
+      </div>
+
         <br>
         <br>
         <b-row fluid>
           <b-col cols="12" md="6" sm="12" align-self="center">
             <b-card>
-                <span class="titulo-card">Envianos un mensaje para consulta de precios, cotizaciones, asesoría e información de nuestros productos.</span>
+                <div class="titulo-card">Déjanos conocerte</div>
                 <contact-form/>
             </b-card>
           </b-col>
           <b-col cols="12" md="6" sm="12" align-self="center">
             <b-card >
-              <span class="titulo-card">Ubicación: Jesús Maria 153 local B, colonia centro  cp 06000 delegacion Cuauhtemoc </span>
+              <div class="titulo-card">Visitanos</div>
               <myMap :coordinates="[19.425618, -99.128960]" :height="map_height"/>
             </b-card>
           </b-col> 
@@ -84,6 +95,53 @@ export default {
     background-color: rgba(255, 255, 255,.75);
     font-weight: 400;
 }
+#container {
+  color: #a13a4b;
+  text-transform: uppercase;
+  font-size: xx-large;
+  font-weight:400;
+  width:100%;
+  bottom:45%;
+  display:block;
+}
+
+#flip {
+  height:50px;
+  overflow:hidden;
+}
+
+#flip > div > div {
+  color:#fff;
+  padding:4px 12px;
+  height:45px;
+  margin-bottom:45px;
+  display:inline-block;
+}
+
+#flip div:first-child {
+  animation: show 5s linear infinite;
+}
+
+#flip div div {
+  background:#42c58a;
+}
+#flip div:first-child div {
+  background:#4ec7f3;
+}
+#flip div:last-child div {
+  background:#DC143C;
+}
+
+@keyframes show {
+  0% {margin-top:-270px;}
+  5% {margin-top:-180px;}
+  33% {margin-top:-180px;}
+  38% {margin-top:-90px;}
+  66% {margin-top:-90px;}
+  71% {margin-top:0px;}
+  99.99% {margin-top:0px;}
+  100% {margin-top:-270px;}
+}
 .titulo {
     display:inline-block;
     overflow:hidden;
@@ -95,6 +153,7 @@ export default {
 .titulo-card{
   font-size: 1.3em;
   font-weight: 600;
+  margin-bottom: 1em;
 }
 @media screen and (max-width: 565px){
       .titulo{
